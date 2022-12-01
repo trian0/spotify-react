5f824943b49a4d9fc77fc25afe1a8a999e1140c4
Thales Rian <thalessantos.hrzon@gmail.com>
2022-12-01 01:44:30 -0300

    feat(App.js): usa App somente para Router

--- src/App.js
+++ src/App.js
@@ -1,104 +1,17 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
 
-  const [token, setToken] = useState("");
-  const [searchKey, setSearchKey] = useState("");
-  const [albuns, setAlbuns] = useState([]);
-  const [tracks, setTracks] = useState([]);
-  const [albumSelected, setAlbumSelected] = useState([]);
-
-  const CLIENT_ID = "a6c10d8c0a4645148d713092c50057db"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
-
-  useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
-
-    if (!token && hash) {
-      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-      window.location.hash = ""
-      window.localStorage.setItem("token", token)
-    }
-
-    setToken(token)
-  }, []);
-
-  const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  }
-
-  const searchAlbuns = async (e) => {
-    e.preventDefault()
-    const { data } = await axios.get("https://api.spotify.com/v1/search", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      params: {
-        q: searchKey,
-        type: "album",
-        limit: 10,
-      }
-    })
-
-    console.log("albums", data.albums.items);
-    console.log("token", token);
-    setAlbuns(data.albums.items)
-  }
-
-  const searchTracks = async (id, albuns) => {
-    setAlbumSelected(albuns)
-    const { data } = await axios.get("https://api.spotify.com/v1/albums/" + id + "/tracks", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-
-    setTracks(data.items)
-  }
-
-  const renderAlbuns = () => {
-    return albuns.map(albuns => (
-      <div key={albuns.id} onClick={() => searchTracks(albuns.id, albuns)}>
-        {albuns.images.length ? <img width={"100%"} src={albuns.images[0].url} alt="" /> : <div>No Image</div>}
-        {albuns.name}
-      </div>
-    ))
-  }
-
-  const renderTracks = () => {
-    return tracks.map(tracks => (
-      <div key={tracks.id}>
-        {tracks.name}
-      </div>
-    ))
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Spotifyzon </h1>
-        {!token ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}> Logar </a>
-          : <button onClick={logout}> Sair </button>}
-        {token && !tracks ?
-          <form onSubmit={searchAlbuns}>
-            <input type="text" onChange={e => setSearchKey(e.target.value)} />
-            <button type={"submit"}>Search</button>
-            {renderAlbuns()}
-          </form> : tracks && token ?
-            <form className="App-form">
-              {albumSelected.images.length ? <img width={"100%"} src={albumSelected.images[0].url} alt="" /> : <div>No Image</div>}
-              <form>{renderTracks()}</form>
-            </form> : <h2>Por favor, faça o login!</h2>
-        }
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
